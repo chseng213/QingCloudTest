747b7f8399fc6a20edf53c33525c7ee22a9d308d
chseng <chseng213@163.com>
2021-01-30 15:52:19 +0800

    fix:Convert the exception class to the message class

--- main/utils/errors.py
+++ main/utils/errors.py
@@ -1,47 +1,41 @@
 # encoding: utf-8
+"""
+ErrorCode  and  meanings
+-1  unknown Error
+1   config file not found
+2   config params not contain   qy_secret_access_keya and  qy_access_key_id and  zone
+3   The configured params must be a string type
+4   json args error
+5   TerminateInstances action should specify one or more instances id'
+"""
 
 
-class RequestsException(Exception):
+class ExitMessage(object):
+    def __init__(self, error_code, error_info):
+        self.error_code = error_code
+        self.error_info = error_info
+
+    def __str__(self):
+        return 'The program exits abnormally. ErrorCode:%s\tErrorMessage:%s' % (self.error_code, self.error_info)
+
+    def __repr__(self):
+        return 'The program exits abnormally. ErrorCode:%s\tErrorMessage:%s' % (self.error_code, self.error_info)
+
+
+class RequestsException(ExitMessage):
     """
     request exception to receive error code and msg
     """
-
-    def __init__(self, error_code, error_info):
-        """
-
-        :param error_code: last time requests code  or api return code
-        :param error_info: error information   or  api  return message
-        """
-        self.error_code = error_code
-        self.error_info = error_info
-
-    def __str__(self):
-        return 'ErrorCode:%s\tErrorMessage:%s' % (self.error_code, self.error_info)
+    pass
 
 
-class ConfigException(Exception):
-    def __init__(self, error_info, error_code=-1):
-        """
-
-        :param error_code:
-        :param error_info: error information
-        """
-        self.error_code = error_code
-        self.error_info = error_info
-
-    def __str__(self):
-        return 'ErrorCode:%s\tErrorMessage:%s' % (self.error_code, self.error_info)
+class ConfigException(ExitMessage):
+    pass
 
 
-class JsonArgsException(Exception):
-    def __init__(self, error_info, error_code=-2):
-        """
+class JsonArgsException(ExitMessage):
+    pass
 
-        :param error_code:
-        :param error_info: error information
-        """
-        self.error_code = error_code
-        self.error_info = error_info
 
-    def __str__(self):
-        return 'ErrorCode:%s\tErrorMessage:%s' % (self.error_code, self.error_info)
+class BadParameterException(ExitMessage):
+    pass
